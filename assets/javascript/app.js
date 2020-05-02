d0890c0a3d87e8beb5567e950a4e3b05f1316e54
William Gonzales <wgonz94@yahoo.com>
2020-05-01 21:19:07 -0700

    current time function and update clock funciton created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,3 +38,58 @@ let showCurrentTime = () => {
 
     clock.innerText = clockTime;
 }
+
+let updateClock = () => {
+    let time = new Date().getHours();
+    let messageText;
+   
+    let image = "https://media.giphy.com/media/tn8zWeNYA73G0/giphy.gif" //standard image 
+    let timeEventJS = document.getElementById("timeEvent");
+    let clockImageJS = document.getElementById("clock");
+
+    //look into switch statements
+
+    if(time == snoozeit) {
+
+        image = "https://media.giphy.com/media/l49JSxywhF1hgU9ck/giphy.gif";
+        messageText = "Snoozing it!";
+
+    } else if (time == wakeuptime) {
+
+        image = "https://media.giphy.com/media/DrJm6F9poo4aA/giphy.gif"
+        messageText = "Wake up!!";
+
+    } else if (time == lunchtime) {
+
+        image = "https://media.giphy.com/media/l0MYwONBGDS7aPGOk/giphy.gif";
+        messageText = "Let's enjoy some lunch!";
+
+    } else if (time == naptime) {
+
+        image = "https://media.giphy.com/media/l1J3NB0CwlBT01GXS/giphy.gif";
+        messageText = "Nap time!"
+
+    } else if ( time < noon) {
+
+        image = "https://media.giphy.com/media/UQhBA6EhaqTM0K1Gvm/giphy.gif";
+        messageText = "Good Morning!"
+
+    } else if (time >= evening) {
+
+        image = "https://media.giphy.com/media/KcbXFenYh6CEGPl1yQ/giphy.gif";
+        messageText = "Good Evening!";
+
+    } else {
+        image = "https://media.giphy.com/media/tn8zWeNYA73G0/giphy.gif";
+        messageText = "Good afternoon"
+    }
+
+    console.log(messageText);
+    timeEventJS.innerText = messageText;
+    clock.src = image;
+
+    showCurrentTime();
+};
+updateClock();
+
+
